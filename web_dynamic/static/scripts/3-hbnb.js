b5f352d6017bf2056d5278b88745c1fc7749fdf0
wilstermanz <wilstermanz@gmail.com>
2022-12-10 16:47:47 -0600

    Task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,65 @@
+// Lists checked amenities
+$(function () {
+  const checkedAmenities = [];
+  const names = [];
+  $('li :checkbox').change(function () {
+    if (this.checked) {
+      checkedAmenities.push($(this).attr('data-id'));
+      names.push($(this).attr('data-name'));
+    } else {
+      const indexID = names.indexOf($(this).attr('data-id'));
+      const indexName = names.indexOf($(this).attr('data-name'));
+      checkedAmenities.splice(indexID, 1);
+      names.splice(indexName, 1);
+    }
+    $('.amenities h4').html(names.join(', '));
+  });
+});
+
+// Checks api status
+$(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
+
+// This loads in everything uing only the api
+$(document).ready(function() {
+  const sendData = {};
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    data: JSON.stringify(sendData),
+    contentType: 'application/json',
+    dataType: 'json',
+    async: false,
+    success: function (places) {
+      for (const place of places) {
+        $.get(`http://0.0.0.0:5001/api/v1/users/${place.user_id}`, function (user) {
+          const html = `<article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guest${place.max_guest === 1 ? 's' : ''}</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms === 1 ? 's' : ''}</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms === 1 ? 's' : ''}</div>
+              </div>
+              <div class="user">
+                <b>Owner:</b> ${user.first_name} ${user.last_name}
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>`;
+          $('.places').append(html);
+        });
+      }
+    }
+  });
+});
