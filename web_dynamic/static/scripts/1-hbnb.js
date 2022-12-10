7e6839b4920258eeb27466870fe10d64e42ae6b4
wilstermanz <wilstermanz@gmail.com>
2022-12-10 13:31:54 -0600

    Updated 1-js to show names instead of ids when checked

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,16 @@
 $(function () {
   const checkedAmenities = [];
+  const names = [];
   $('li :checkbox').change(function () {
     if (this.checked) {
       checkedAmenities.push($(this).attr('data-id'));
+      names.push($(this).attr('data-name'));
     } else {
-      checkedAmenities.splice($.inArray($(this).attr('data-id'), checkedAmenities), 1);
+      const indexID = names.indexOf($(this).attr('data-id'));
+      const indexName = names.indexOf($(this).attr('data-name'));
+      checkedAmenities.splice(indexID, 1);
+      names.splice(indexName, 1);
     }
-    $('.amenities h4').html(checkedAmenities.join(', '));
+    $('.amenities h4').html(names.join(', '));
   });
 });
