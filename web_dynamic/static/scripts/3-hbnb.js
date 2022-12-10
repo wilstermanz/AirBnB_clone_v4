c423c0f93cf2e6e606a43d7ed516511de80b7f60
wilstermanz <wilstermanz@gmail.com>
2022-12-10 17:12:38 -0600

    Fixed so only one API call is needed in order to get the list of users.

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -29,37 +29,38 @@ $(function () {
 
 // This loads in everything uing only the api
 $(document).ready(function() {
-  const sendData = {};
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     type: 'POST',
-    data: JSON.stringify(sendData),
+    data: '{}',
     contentType: 'application/json',
     dataType: 'json',
     async: false,
     success: function (places) {
-      for (const place of places) {
-        $.get(`http://0.0.0.0:5001/api/v1/users/${place.user_id}`, function (user) {
-          const html = `<article>
-              <div class="title_box">
-                <h2>${place.name}</h2>
-                <div class="price_by_night">$${place.price_by_night}</div>
-              </div>
-              <div class="information">
-                <div class="max_guest">${place.max_guest} Guest${place.max_guest === 1 ? 's' : ''}</div>
-                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms === 1 ? 's' : ''}</div>
-                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms === 1 ? 's' : ''}</div>
-              </div>
-              <div class="user">
-                <b>Owner:</b> ${user.first_name} ${user.last_name}
-              </div>
-              <div class="description">
-                ${place.description}
-              </div>
-            </article>`;
-          $('.places').append(html);
-        });
-      }
+      $.get('http://0.0.0.0:5001/api/v1/users/', function (users) {
+        for (const place of places) {
+          const user = users.filter(user => {
+            return user.id === place.user_id;
+          })[0];
+          $('.places').append(`<article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+          </div>
+          <div class="user">
+            <b>Owner:</b> ${user.first_name} ${user.last_name}
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`);
+        }
+      });
     }
   });
 });
